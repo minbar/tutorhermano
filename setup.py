37f025e59a54dab53a826290548d2172cffc1e1f
barcauskas <mindaugas.barcauskas@gmail.com>
2024-04-25 22:29:38 +0300

    renaming more

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ setup(
     python_requires=">=3.8",
     install_requires=["tutor>=17.0.0,<18.0.0", "tutor-mfe>=17.0.0,<18.0.0"],
     extras_require={"dev": "tutor[dev]>=17.0.0,<18.0.0"},
-    entry_points={"tutor.plugin.v1": ["indigo = tutorhermano.plugin"]},
+    entry_points={"tutor.plugin.v1": ["hermano = tutorhermano.plugin"]},
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
